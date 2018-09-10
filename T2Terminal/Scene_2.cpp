36bdbec1816e79be9f21c8057b7993f43c9aefb9
Rushabh Parakh <rparakh@nvidia.com>
2018-09-10 16:42:14 +0530

    Commiting minor scene1 and scene2 changes

--- T2Terminal/Scene_2.cpp
+++ T2Terminal/Scene_2.cpp
@@ -59,8 +59,8 @@ void Scene_2::Scene_2::Initialize()
 
 	_spotLight->Initialize();
 	_spotLightBox->Initialize();
-	//_particles->Initialize();
-	//_rollingCylinder->Initialize();
+	_particles->Initialize();
+	_rollingCylinder->Initialize();
 	_bluePrint->Initialize();
 	_scene2Tile->Initialize();
 	_wireFrameEffect->Initialize();
@@ -77,8 +77,8 @@ void Scene_2::Scene_2::Update()
 
 	_spotLight->Update();
 	_spotLightBox->Update();
-    //_particles->Update();
-	//_rollingCylinder->Update();
+    _particles->Update();
+	_rollingCylinder->Update();
 	_bluePrint->Update();
 	_airportTop->Update();
 
@@ -89,8 +89,8 @@ void Scene_2::Scene_2::ReSize(int width, int height, struct ResizeAttributes att
 {
 	_spotLight->ReSize(width, height, attributes);
 	_spotLightBox->ReSize(width, height,attributes);
-	//_particles->ReSize(width, height, attributes);
-	//_rollingCylinder->ReSize(width, height, attributes);
+	_particles->ReSize(width, height, attributes);
+	_rollingCylinder->ReSize(width, height, attributes);
 	_bluePrint->ReSize(width, height, attributes);
 	_scene2Tile->ReSize(width, height, attributes);
 	_wireFrameEffect->ReSize(width, height, attributes);
@@ -105,7 +105,7 @@ void Scene_2::Scene_2::Render(HDC hdc, struct Attributes attributes)
 	_spotLight->Render(hdc, attributes);
 
 	_scene2Tile->Render(hdc, attributes);
-	//_rollingCylinder->Render(hdc, attributes);
+	_rollingCylinder->Render(hdc, attributes);
 	_bluePrint->Render(hdc, attributes);
 	if (attributes.currentTransformation == TRANSFORMATION_START_WIRE_FRAME)
 	{
@@ -114,7 +114,7 @@ void Scene_2::Scene_2::Render(HDC hdc, struct Attributes attributes)
 	}
 	else
 	{
-		//_particles->Render(hdc, attributes);
+		_particles->Render(hdc, attributes);
 		_spotLightBox->Render(hdc, attributes);
   }
 }
@@ -123,8 +123,8 @@ void Scene_2::Scene_2::SceneTransition()
 {
 	_spotLight->SceneTransition();
 	_spotLightBox->SceneTransition();
-	//_rollingCylinder->SceneTransition();
-	//_particles->SceneTransition();
+	_rollingCylinder->SceneTransition();
+	_particles->SceneTransition();
 	_bluePrint->SceneTransition();
 	_wireFrameEffect->SceneTransition();
 	_airportTop->SceneTransition();
