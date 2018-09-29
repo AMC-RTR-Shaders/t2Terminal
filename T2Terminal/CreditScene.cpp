4639a282db1f62dd62c22306fc2ffa74044f9fa5
Harsh Patel <harshpatel00789@gmail.com>
2018-09-29 15:51:35 +0530

    Changed Final track

--- T2Terminal/CreditScene.cpp
+++ T2Terminal/CreditScene.cpp
@@ -373,8 +373,8 @@ void Sanket::CreditScene::Render(HDC hdc, struct Attributes attributes)
 	}
 	else if (vertexY <= 21.0f)
 	{
-		texCoordY += 0.05f / (42.0f);
-		vertexY += 0.05f;
+		texCoordY += 0.1f / (42.0f);
+		vertexY += 0.1f;
 	}
 	else
 	{
